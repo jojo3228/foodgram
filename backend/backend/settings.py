144dec4edd9af0d11723edbf9a6b0d24f2c58ed9
jojo3228 <nikita_pavlov_90@mail.ru>
2024-08-20 18:27:25 +0300

    Final8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -152,7 +152,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
 STATIC_URL = '/backend_static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
