84db8ac9af403f3f20c9a5308c9b82e543e061d3
jojo3228 <nikita_pavlov_90@mail.ru>
2024-08-23 00:14:50 +0300

    Ver10

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,5 @@
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
+python manage.py loaddata data/ingredients.json --no-input
 gunicorn --bind 0.0.0.0:8000 backend.wsgi
